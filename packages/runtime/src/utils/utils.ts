24e572a3ed348f6399f9ece8942ee62a9ab3fd91
欧阳鑫 <254225961@qq.com>
2020-07-24 17:41:11 +0800

    feat: util

--- packages/runtime/src/utils/utils.ts
+++ packages/runtime/src/utils/utils.ts
@@ -1,8 +1,66 @@
-
 export function format(first: string, middle: string, last: string): string {
   return (
-    (first || '') +
-    (middle ? ` ${middle}` : '') +
-    (last ? ` ${last}` : '')
+    (first || "") + (middle ? ` ${middle}` : "") + (last ? ` ${last}` : "")
   );
+}
+
+export type TouchTargetEvent = { target: HTMLElement } & TouchEvent;
+
+export function same<T>(props: T, value: any): value is T {
+  return props === value;
+}
+
+/**
+ * 将事件发送给原生
+ * @param bindFunctionName
+ * @param data
+ */
+export function eventToNative(bindFunctionName, payload) {
+  const webkit = Reflect.get(window, "webkit");
+  webkit.messageHandlers.triggerEvent.postMessage(
+    JSON.stringify({
+      method: bindFunctionName,
+      payload
+    })
+  );
+}
+
+export function getTouchs(touchs: TouchList) {
+  return Array.from(touchs ?? []).map(item => ({
+    clientX: item.clientX,
+    clientY: item.clientY,
+    force: item.force,
+    identifier: item.identifier,
+    pageX: item.pageX,
+    pageY: item.pageY
+  }));
+}
+
+/**
+ * 同步小程序事件参数
+ * @param event
+ */
+export function createEvent(event: TouchTargetEvent) {
+  const touches = getTouchs(event.touches);
+  const changedTouches = getTouchs(event.changedTouches);
+
+  const target = {
+    offsetLeft: event.target?.offsetLeft,
+    offsetTop: event.target?.offsetTop,
+    dataset: { ...event.target?.dataset }
+  };
+
+  return {
+    type: event.type,
+    timeStamp: Date.now(),
+    detail: {
+      x: touches[0]?.pageX,
+      y: touches[0]?.pageY
+    },
+    target,
+    currentTarget: target,
+    changedTouches,
+    touches,
+    _requireActive: true
+  };
 }
