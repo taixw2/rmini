fee0c06a7ba2714fcfec835b200779d72b863c92
欧阳鑫 <254225961@qq.com>
2020-07-31 10:53:30 +0800

    style:  style compiler-html(javascript)

--- packages/compiler/compiler-html/compiler-javascript.js
+++ packages/compiler/compiler-html/compiler-javascript.js
@@ -1,28 +1,50 @@
 const reg = /\{\{([\.|\w|\d]+?)\}\}/g;
 
+/**
+ * pre script
+ *
+ * 在 Vue 中会获取该 __data__ 作为响应式对象
+ */
+const prefixScript = `
+window.__DATA__ = {}
+window.__Def = function (key, value) {
+  if (typeof window.__DATA__[key] === 'object') {
+    window.__DATA__[key] = {...window.__DATA__[key], ...value}
+  }
+  window.__DATA__[key] = value
+};
+`;
+
+/**
+ *
+ *
+ * @param {*} interpolationExpression
+ */
 function recombinObject(interpolationExpression) {
-  const list = [];
-  let prekey = null;
-  interpolationExpression.split(".").forEach((item) => {
-    if (!list.length) {
-      prekey = item;
-      list.push({ [item]: {} });
-      return;
-    }
-    const top = list[list.length - 1];
-    top[prekey][item] = {};
-    return;
-  });
-  return list[0];
+  let root = (current = {});
+  const keys = interpolationExpression.split(".");
+  const len = keys.length;
+  for (let index = 0; index < len; index++) {
+    const key = keys[index];
+    current[key] = len === index + 1 ? "" : {};
+    current = current[key];
+  }
+  return [keys[0], root[keys[0]]];
 }
 
+/**
+ * 提取所有插值表达式中用到的响应式属性
+ * 通过 __Def 传给 runtime 中的 Vue 实例
+ * @param {*} wxmlContent 
+ */
 exports.compilerJavascript = function(wxmlContent) {
-  const data = {};
+  let script = prefixScript;
+
   while (true) {
     const result = reg.exec(wxmlContent);
     if (!result) break;
-    Object.assign(data, recombinObject(result[1]))
+    const [key, value] = recombinObject(result[1]);
+    script += `__Def(${JSON.stringify(key)}, ${JSON.stringify(value)});`;
   }
-
-  return `${Reflect.ownKeys(data).map(key => `__Def(${JSON.stringify(key)}, ${JSON.stringify(data[key])});`).join('\n')}`
+  return script;
 };
--- packages/compiler/compiler-html/index.js
+++ packages/compiler/compiler-html/index.js
@@ -11,7 +11,7 @@ const template = {
   <html lang="en" style="font-size: __FONT_SIZE__px">
   <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="viewport" content="width=device-width,user-scalable=no,initial-scale=—__SCALE__,maximum-scale=—__SCALE__,minimum-scale=—__SCALE__">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
   `,
   body: `
@@ -41,7 +41,7 @@ exports.compilerHtml = function(entryDir, appConfig, projectConfig) {
 
     const originalWxmlContent = await fs.promises.readFile(wxmlPath, { encoding: "utf-8" });
     const styleContent = await compilerStyle(pageEntry);
-    const scriptContent = await compilerJavascript(originalWxmlContent);
+    const scriptContent = compilerJavascript(originalWxmlContent);
     const wxmlContent = await compilerHtml(originalWxmlContent)
 
     let content = "";
