3f207344952d51a116e6fd35a24dcf9ad46d575d
mro <fex@0000o.net>
2020-08-01 16:58:48 +0800

    feat: mock console

--- packages/compiler/compiler-javascript/polyfill/entry.js
+++ packages/compiler/compiler-javascript/polyfill/entry.js
@@ -1,3 +1,4 @@
+import { invoke } from "./jsbridge";
 
 /**
  * 导出给小程序使用的方法
@@ -5,6 +6,23 @@
 export { getApp, wx, App, Page } from "./polyfill";
 
 /**
+ * native invoke
+ */
+export { nativeInvoke } from "./export-native";
+
+/**
  * global
  */
-export const global = {}
+export const global = {};
+
+export const console = {
+  info(...msg) {
+    invoke("console", false, "", ["info:", ...msg]);
+  },
+  log(...msg) {
+    invoke("console", false, "", msg);
+  },
+  error(...msg) {
+    invoke("console", false, "", ["error: ", ...msg]);
+  },
+};
